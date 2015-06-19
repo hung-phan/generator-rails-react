fe3a7cef652e48cb1d7ab265b6ceca8d7287d024
hung-phan <colorvisavn@gmail.com>
2015-06-19 19:14:35 +0700

    initialize guard rspec

--- app/index.js
+++ app/index.js
@@ -247,6 +247,10 @@ var RailsReactGenerator = yeoman.generators.Base.extend({
     shell.exec("rails generate rspec:install");
   },
 
+  guardRspec: function() {
+    shell.exec("guard init rspec");
+  },
+
   defaultStylesheet: function() {
     console.log(magenta('Copy default.css.scss file'));
     this.template('app/default.css.scss', 'app/assets/stylesheets/default.css.scss');
